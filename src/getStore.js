30291cd8d5cf4af0ef4fe338e3c305772866c6b6
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-05-14 18:18:12 -0300

    Configuring Redux Saga

--- src/getStore.js
+++ src/getStore.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import { Iterable } from 'immutable';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import { getQuery } from './utility';
 import { reducer } from './combineReducers';
@@ -17,12 +18,16 @@ const logger = createLogger({
 });
 
 export const getStore = () => {
-  const middleWares = [thunk];
+  const sagaMiddleware = createSagaMiddleware();
+  const middleWares = [sagaMiddleware, thunk];
+
   if (getQuery()['logger']) {
     middleWares.push(logger);
   }
+
   const composables = [applyMiddleware(...middleWares)];
   const enhancer = compose(...composables);
   const store = createStore(reducer, defaultState, enhancer);
+
   return store;
 };
