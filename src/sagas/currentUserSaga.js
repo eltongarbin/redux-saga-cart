fade878f34637f8a07765dffe38a6e956af027ce
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-05-15 21:37:50 -0300

    Implementing Take, Call and Put

--- src/sagas/currentUserSaga.js
+++ src/sagas/currentUserSaga.js
@@ -1,8 +1,12 @@
-import { delay } from 'redux-saga';
+import { take, put, call, apply } from 'redux-saga/effects';
+import fetch from 'isomorphic-fetch';
 
-export function* currentUserSaga(params) {
-  while (true) {
-    yield delay(1000);
-    console.info('User saga loop');
-  }
+import { GET_CURRENT_USER_INFO, setCurrentUser } from './../actions';
+
+export function* currentUserSaga() {
+  const { id } = yield take(GET_CURRENT_USER_INFO);
+  const response = yield call(fetch, `http://localhost:8081/user/${id}`);
+  const data = yield apply(response, response.json);
+
+  yield put(setCurrentUser(data));
 }
